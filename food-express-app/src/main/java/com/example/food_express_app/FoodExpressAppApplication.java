b6bdc625e9e0d45090a7687a2647d9f178eced67
Neeraj Upadhyay <96068816+NeerajUpadhyay01@users.noreply.github.com>
2024-12-26 20:59:48 +0530

    Add files via upload
    project initialization

---
+++ food-express-app/src/main/java/com/example/food_express_app/FoodExpressAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.food_express_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class FoodExpressAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodExpressAppApplication.class, args);
+	}
+
+}
