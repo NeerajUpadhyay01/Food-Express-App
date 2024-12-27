7d86b6512adddd9c93f4a5c5c29eb2702b4d646d
Neeraj <neerajsaraswat111@gmail.com>
2024-12-27 11:59:21 +0530

    Add HTML templates for home and login pages, update application properties, and style with CSS

--- food-express-app/src/main/java/com/example/food_express_app/FoodExpressAppApplication.java
+++ food-express-app/src/main/java/com/example/food_express_app/FoodExpressAppApplication.java
@@ -2,10 +2,10 @@ package com.example.food_express_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
+// @EnableWebMvc
 public class FoodExpressAppApplication {
 
 	public static void main(String[] args) {
