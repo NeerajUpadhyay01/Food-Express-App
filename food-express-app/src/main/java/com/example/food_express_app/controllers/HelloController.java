0b16b3aae84e0965e457d8520d44920a0f6ef62a
Neeraj <neerajsaraswat111@gmail.com>
2024-12-27 08:25:54 +0530

    Refactor HelloController and update HTML template structure

---
+++ food-express-app/src/main/java/com/example/food_express_app/controllers/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.food_express_app.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/app")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greet(@RequestParam String name, Model model) {
+        model.addAttribute("uname", name);
+        model.addAttribute("user", "donjoe");
+        model.addAttribute("location", "hyderabad");
+        return "hello world";
+    }
+}
